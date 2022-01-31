1d23fd39f6e3624a4395329a3ed3c230bdbaf47d
Gesualdo14 <mgesualdo14@gmail.com>
2022-01-30 22:19:32 -0300

    Cambios

--- src/components/products/ProductsList.js
+++ src/components/products/ProductsList.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { uiOpenModal } from '../../actions/ui'
-import { getProducts } from '../../actions/products'
+import { fetchSinToken } from '../../helpers/fetch'
 import { emptyProduct } from '../../utils/constants'
+import Spinner from '../ui/Spinner'
 import { ProductNoveltyModal } from './ProductNoveltyModal'
 import './productsList.css'
+
+const baseUrl = process.env.REACT_APP_API_URL
 
 const ProductsList = ({
   setAddingProduct,
@@ -15,6 +18,8 @@ const ProductsList = ({
   const { products } = useSelector((state) => state.product)
   const [selectedProduct, setSelectedProduct] = useState()
   const [searchText, setSearchText] = useState()
+  const [loading, setLoading] = useState()
+  const [novelties, setNovelties] = useState()
 
   const handleAddProduct = () => {
     setAddingProduct(true)
@@ -27,9 +32,16 @@ const ProductsList = ({
 
   const dispatch = useDispatch()
 
-  const handleEditProduct = ({ _id, name, price, description, images }) => {
+  const handleEditProduct = async ({
+    _id,
+    name,
+    price,
+    description,
+    images,
+  }) => {
     setAddingProduct(false)
     console.log('LA PUTA QUE TE PARIO!!')
+    setSelectedProduct({ _id, name, price, description, images })
     setNewProduct({
       _id,
       name,
@@ -45,6 +57,14 @@ const ProductsList = ({
     })
     setPreviewImageUrls(newPreviewImageUrls)
     setAvatarFiles([])
+    const url = `product/novelty/${_id}`
+    setLoading(true)
+    const res = await fetchSinToken(url)
+    const body = await res.json()
+    setLoading(false)
+    if (body.ok) {
+      setNovelties(body.data)
+    }
   }
 
   const handleAddProductNovelty = (product) => {
@@ -52,6 +72,7 @@ const ProductsList = ({
     setSelectedProduct(product)
   }
 
+  console.log({ selectedProduct })
   return (
     <div className='products-container'>
       <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
@@ -104,54 +125,107 @@ const ProductsList = ({
           return p.name.toLowerCase().includes(searchText.toLowerCase())
         })
         .map((product) => (
-          <div key={product._id} className='product-detail-container'>
-            <div style={{ width: '50%' }}>
-              <img
-                src={`https://appturnos.blob.core.windows.net/productos/${
-                  product.images[0]
-                }?${new Date().getTime()}`}
-                alt=''
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <div key={product._id} className='product-detail-container'>
+              <div style={{ width: '50%' }}>
+                <img
+                  src={`https://appturnos.blob.core.windows.net/productos/${
+                    product.images[0]
+                  }?${new Date().getTime()}`}
+                  alt=''
+                  style={{
+                    width: '2.5rem',
+                    height: '2.5rem',
+                    marginRight: '0.7rem',
+                    borderRadius: '2.5rem',
+                    objectFit: 'contain',
+                  }}
+                />
+                <span>{product.name}</span>
+              </div>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <b style={{ marginRight: '0.5rem' }}>
+                  <u>Stock </u>:
+                </b>
+                <span>
+                  {product.purchases +
+                    product.devolutions -
+                    product.sales -
+                    product.losts}
+                </span>
+              </div>
+              <div
                 style={{
-                  width: '2.5rem',
-                  height: '2.5rem',
-                  marginRight: '0.7rem',
-                  borderRadius: '2.5rem',
-                  objectFit: 'contain',
+                  display: 'flex',
+                  width: '33%',
+                  justifyContent: 'flex-end',
                 }}
-              />
-              <span>{product.name}</span>
-            </div>
-            <div style={{ display: 'flex', alignItems: 'center' }}>
-              <b style={{ marginRight: '0.5rem' }}>
-                <u>Stock </u>:
-              </b>
-              <span>
-                {product.purchases +
-                  product.devolutions -
-                  product.sales -
-                  product.losts}
-              </span>
-            </div>
-            <div
-              style={{
-                display: 'flex',
-                width: '33%',
-                justifyContent: 'flex-end',
-              }}
-            >
-              <button
-                className='btn btn-warning mr-3'
-                onClick={() => handleEditProduct(product)}
               >
-                <i className='fas fa-edit'></i>
-              </button>
-              <button
-                className='btn btn-info mr-1'
-                onClick={() => handleAddProductNovelty(product)}
-              >
-                <i className='fas fa-shopping-cart'></i>
-              </button>
+                <button
+                  className='btn btn-warning mr-3'
+                  onClick={() => handleEditProduct(product)}
+                >
+                  <i className='fas fa-edit'></i>
+                </button>
+                <button
+                  className='btn btn-info mr-1'
+                  onClick={() => handleAddProductNovelty(product)}
+                >
+                  <i className='fas fa-shopping-cart'></i>
+                </button>
+              </div>
             </div>
+            {loading && selectedProduct?._id === product._id && (
+              <Spinner changeColor />
+            )}
+            {selectedProduct?._id === product._id && novelties?.length > 0 && (
+              <div>
+                <h4 style={{ marginBottom: '0.5rem' }}>
+                  <u>Últimas novedades</u>
+                </h4>
+                {novelties?.map((n) => (
+                  <div
+                    style={{
+                      display: 'flex',
+                      flexDirection: 'column',
+                      alignItems: 'flex-start',
+                      justifyContent: 'flex-start',
+                      backgroundColor: 'white',
+                      borderRadius: '0.3rem',
+                      padding: '0.5rem',
+                      width: '100%',
+                      marginBottom: '1rem',
+                      boxShadow: '0 0 5px 2px rgba(0,0,0,0.2)',
+                    }}
+                  >
+                    <div
+                      style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'space-between',
+                        backgroundColor: 'white',
+                        borderRadius: '0.3rem',
+                        width: '100%',
+                      }}
+                    >
+                      <span>
+                        <b>{n.type}</b>
+                      </span>
+                      <span>{n.quantity}</span>
+                      <span>${n.unitPrice}</span>
+                    </div>
+                    {n?.details && (
+                      <div style={{ width: '100%', marginTop: '0.5rem' }}>
+                        <p style={{ margin: '0' }}>
+                          <u>Detalle</u>:
+                        </p>
+                        <span>{n.details}</span>
+                      </div>
+                    )}
+                  </div>
+                ))}
+              </div>
+            )}
           </div>
         ))}
       <ProductNoveltyModal selectedProduct={selectedProduct} />
